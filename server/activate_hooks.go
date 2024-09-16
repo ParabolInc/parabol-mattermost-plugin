53f7a80ceb52d18c426e528073d89a1310805cda
Georg Bremer <github@dschoordsch.de>
2024-09-16 17:57:42 +0200

    Init from demo plugin

---
+++ server/activate_hooks.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/pkg/errors"
+)
+
+// OnActivate is invoked when the plugin is activated.
+//
+// This demo implementation logs a message to the demo channel whenever the plugin is activated.
+// It also creates a demo bot account
+func (p *Plugin) OnActivate() error {
+	if err := p.registerCommands(); err != nil {
+		return errors.Wrap(err, "failed to register commands")
+	}
+
+	return nil
+}
+
+// OnDeactivate is invoked when the plugin is deactivated. This is the plugin's last chance to use
+// the API, and the plugin will be terminated shortly after this invocation.
+//
+// This demo implementation logs a message to the demo channel whenever the plugin is deactivated.
+func (p *Plugin) OnDeactivate() error {
+	return nil
+}
+
---
+++ webapp/src/index.tsx
@@ -0,0 +1,33 @@
+import {Store, Action} from 'redux';
+
+import {GlobalState} from '@mattermost/types/lib/store';
+
+import manifest from '@/manifest';
+
+import {PluginRegistry} from '@/types/mattermost-webapp';
+
+export default class Plugin {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+    public async initialize(registry: PluginRegistry, store: Store<GlobalState, Action<Record<string, unknown>>>) {
+        // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+                registry.registerChannelHeaderButtonAction(
+            // icon - JSX element to use as the button's icon
+            'Should be an icon',//<Icon />,
+            // action - a function called when the button is clicked, passed the channel and channel member as arguments
+            // null,
+            () => {
+                alert("Hello World!");
+            },
+            // dropdown_text - string or JSX element shown for the dropdown button description
+            "Hello World",
+        );
+    }
+}
+
+declare global {
+    interface Window {
+        registerPlugin(pluginId: string, plugin: Plugin): void
+    }
+}
+
+window.registerPlugin(manifest.id, new Plugin());
